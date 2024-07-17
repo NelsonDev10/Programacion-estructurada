99c5418786c7157ddd18fcdd81d506e10fd4697a
NelsonDev10 <nelsitorodriguez2006@gmail.com>
2024-07-17 09:55:59 -0600

    Agregando una nueva variable

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const username = "user"
+
+const name = "nelson"
+
+
+//primero agregamos una nuevo request con git add .
+
+//luego agregamos un comentario con git commit -m "tu comentario"
+
+//finalmente apuntamos a la rama que queremos subir nuestro nuevos cambios con git push -u origin main
